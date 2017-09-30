22a5d771bdadff87a8a412bbf807ef3fc3177ff1
unknown <madziorgadzior@gmail.com>
2017-09-30 09:36:50 +0200

    JohnCena

---
+++ JohnCena/app/src/main/java/pl/koisuru/johncena/Regulamin.java
@@ -0,0 +1,30 @@
+package pl.koisuru.johncena;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Regulamin extends AppCompatActivity {
+    private Button przycisk;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.regulamin);
+
+        przycisk =(Button) findViewById(R.id.przy);
+
+        przycisk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"zaakceptowano",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+    }
+
+}
